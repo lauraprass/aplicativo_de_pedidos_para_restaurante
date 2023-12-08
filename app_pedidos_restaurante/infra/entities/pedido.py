c409377e103ed24a9f2b32519efaa955fa6914a9
Matheus-A-Barboza <106258751+Matheus-A-Barboza@users.noreply.github.com>
2023-12-07 23:49:44 -0300

    Updating the "infra" folder

--- app_pedidos_restaurante/infra/entities/pedido.py
+++ app_pedidos_restaurante/infra/entities/pedido.py
@@ -1,15 +1,10 @@
 from __future__ import annotations
-from typing import List
-from sqlalchemy import Column, ForeignKey, Table, DateTime
+from datetime import datetime
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship, Mapped, mapped_column
-from infra.config.base import Base
-
 
 class Pedido(Base):
     __tablename__ = 'pedido'
 
-    id = Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    num_pedido = Mapped[int] = mapped_column(nullable=False)
-    itens_pedido = relationship("ItemMenuPedido", backref="pedido")
-    total_pedido = Mapped[float] = mapped_column(nullable=False)
-    status = Mapped[str] = mapped_column(nullable=False)
+    mesa_id: Mapped[int] = mapped_column(ForeignKey("mesas.id"), primary_key=True)
+    
